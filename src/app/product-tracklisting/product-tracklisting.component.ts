db5bccd9b73f34f011aa1d492c4a80adf9ec753d
candyagnes <59609286+candyagnes@users.noreply.github.com>
2020-10-08 12:50:52 +0300

    changes made

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -3,17 +3,18 @@ import { AlbumStorePage } from '../../../e2e/app.po';
 import {Album} from '../album';
 import {ProductService} from '../product.service';
 
-class albumInfo{Album}
+
 @Component({
   selector: 'app-product-tracklisting',
   templateUrl: './product-tracklisting.component.html',
   styleUrls: ['./product-tracklisting.component.css']
 })
 export class ProductTracklistingComponent implements OnInit {
-
-  constructor() { }
+albumInfo:Album;
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response=>this.albumInfo = response);
   }
 
 }
